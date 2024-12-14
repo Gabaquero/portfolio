81c69dd45ecf441be1a1b02568a66e2caca9fac1
Gabaquero <gbaquero76@gmail.com>
2024-12-13 19:34:23 -0600

    Some changes

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -2,31 +2,28 @@ export function trimText(input: string, maxLength: number = 100): string {
   if (input.length <= maxLength) return input;
   return input.substring(0, maxLength - 3) + "...";
 }
-export function getCurrentTimeInItaly(): Date {
+
+export function getCurrentTimeInHouston(): Date {
   // Create a date object with the current UTC time
   const now = new Date();
 
-  // Convert the UTC time to Italy's time
-  const offsetItaly = 2; // Italy is in Central European Summer Time (UTC+2), but you might need to adjust this based on Daylight Saving Time
-  now.setHours(now.getUTCHours() + offsetItaly);
+  // Convert the UTC time to Houston's time (Central Standard Time)
+  const offsetHouston = -6; // Houston is in CST (UTC-6) without DST adjustment
+  now.setHours(now.getUTCHours() + offsetHouston);
 
   return now;
 }
 
-export function formatTimeForItaly(date: Date): string {
+export function formatTimeForHouston(date: Date): string {
   const options: Intl.DateTimeFormatOptions = {
     hour: "numeric",
     minute: "2-digit",
     second: "2-digit",
     hour12: true, // This will format the time in 12-hour format with AM/PM
-    timeZone: "Europe/Rome",
+    timeZone: "America/Chicago", // Houston falls under the "America/Chicago" timezone
   };
 
   let formattedTime = new Intl.DateTimeFormat("en-US", options).format(date);
-
-  // Append the time zone abbreviation. You can automate this with libraries like `moment-timezone`.
-  // For simplicity, here I'm just appending "CET", but do remember that Italy switches between CET and CEST.
-  formattedTime += " CET";
 
   return formattedTime;
 }
